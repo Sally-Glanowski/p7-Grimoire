b1ebf3584a799ce86a4957e652ca577760141b29
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-20 15:07:20 +0200

    users

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,2 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidateur = require("mongoose-unique-validator");
+const userSchema = mongoose.Schema({
+    email: {required: true, unique: true }
+    password: {required: true },
+  });
