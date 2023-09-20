46555dc437597e3f1c8eaad532b03ba4616caf6b
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-20 12:22:44 +0200

    correction email validateur

--- backend/middlewares/validateur-email.js
+++ backend/middlewares/validateur-email.js
@@ -1,9 +1,10 @@
 const emailValidateur = (req, res, next) => {
     const { email } = req.body;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
- if (!email || !email.match(emailRegex)) {
+
+     if (!email || !email.match(emailRegex)) {
       return res.status(400).json({ error: "Adresse mail invalide" });
     }
     return next();
   }; 
-  module.exports = emailValidatur;
+  module.exports = emailValidateur;
