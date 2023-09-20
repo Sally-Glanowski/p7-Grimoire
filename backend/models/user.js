013d41e0aff13fb69f6ddd771c9dd6f4fdb1359a
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-20 15:10:10 +0200

    correction users

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidateur = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
-    email: {required: true, unique: true }
-    password: {required: true },
+    email: { type: String, required: true, unique: true }, 
+    password: {type: String, required: true },
   });
