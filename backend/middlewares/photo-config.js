a040976d00b35d63182153f7089cb4e02b4b9a14
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-22 10:20:02 +0200

    config photos

---
+++ backend/middlewares/photo-config.js
@@ -0,0 +1,32 @@
+const photo = require("photo");
+const fs = require("fs");
+
+const photoconfig = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const fileName = req.file.filename.split(".")[0];
+  const newPath = `${req.file.destination}/${fileName}.webp`;
+
+  try {
+    await photo(req.file)
+      .resize({ width: 400, height: 500, fit: photo.fit.inside })
+      .webp()
+      .toFile(newPath);
+
+    fs.unlink(req.file, (err) => 
+      if (err) {
+        console.error("Erreur lors de la suppression de l'image d'origine:", err);
+      }
+    });
+
+    req.file.filename = `${fileName}.webp`;
+
+    next();
+  } catch (error) {
+    res.status(500).json({ error: "Échec de l'optimisation de l'image" });
+  }
+};
+
+module.exports = photoConfig;
