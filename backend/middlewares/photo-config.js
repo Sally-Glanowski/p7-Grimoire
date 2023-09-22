bb9c77b63f8c25cbac41de1781ffbb91e79ba5b8
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-22 10:29:49 +0200

    correction

--- backend/middlewares/photo-config.js
+++ backend/middlewares/photo-config.js
@@ -10,17 +10,20 @@ const photoconfig = async (req, res, next) => {
   const newPath = `${req.file.destination}/${fileName}.webp`;
 
   try {
-    await photo(req.file)
+    await photo(req.file.path)
       .resize({ width: 400, height: 500, fit: photo.fit.inside })
       .webp()
       .toFile(newPath);
 
-    fs.unlink(req.file, (err) => 
+  
+    fs.unlink(req.file.path, (err) => 
       if (err) {
         console.error("Erreur lors de la suppression de l'image d'origine:", err);
       }
     });
 
+   
+    req.file.path = newPath;
     req.file.filename = `${fileName}.webp`;
 
     next();
