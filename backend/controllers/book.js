c5290d80df6795295879d70703266b0480416f64
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 18:13:04 +0200

    book

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -52,3 +52,87 @@ exports.updateBook = (req, res, next) => {
       }
     : { ...req.body }; // sinon,  utilisation du book existant du corps de la requête
   delete bookObject._userId; // supprime la propriété userid de l'objet livre
+  Book.findOne({ _id: req.params.id })
+  .then((book) => {
+    if (book.userId != req.auth.userId) {
+      res.status(403).json({ message: "403: requête non autorisée" });
+    } else if (req.file) {
+      // Si un nouveau fichier est téléchargé, supprime l'ancien fichier image
+      const filename = book.imageUrl.split("/images")[1];
+      fs.unlink(`images/${filename}`, () => {});
+    }
+    Book.updateOne(
+      { _id: req.params.id }, // 
+      { ...bookObject, _id: req.params.id } // Met à jour le livre correspondant à l'ID
+    )
+      .then(res.status(200).json({ message: "Livre modifié ! " }))
+      .catch((error) => res.status(400).json({ error }));
+  })
+};
+
+exports.deleteBook = (req, res, next) => {
+Book.findOne({ _id: req.params.id })
+  .then((book) => {
+    if (book.userId != req.auth.userId) {
+      res.status(401).json({ message: "Non autorisé" });
+    } else {
+      const filename = book.imageUrl.split("/images/")[1]; // Extrait le nom du fichier de l'URL de l'image du livre
+      fs.unlink(`images/${filename}`, () => {
+        //  le fichier image correspondant du système de fichiers
+        Book.deleteOne({ _id: req.params.id }) // Supprime le livre de la base de données
+          .then(() => {
+            res.status(200).json({ message: "Livre supprimé!" });
+          })
+          .catch((error) => res.status(401).json({ error }));
+      });
+    }
+  })
+  .catch((error) => res.status(500).json({ error }));
+};
+
+exports.rateBook = (req, res, next) => {
+const user = req.body.userId;
+if (user !== req.auth.userId) {
+  res.status(401).json({ message: "Non autorisé" });
+} else {
+  Book.findOne({ _id: req.params.id }) // Recherche le livre par ID
+    .then((book) => {
+      if (book.ratings.find((rating) => rating.userId === user)) {
+        // Vérifie si l'utilisateur a déjà noté le livre
+        res.status(401).json({ message: "Livre déjà noté" });
+      } else {
+        const newRating = {
+          // Crée un objet newRating
+          userId: user,
+          grade: req.body.rating,
+          _id: req.body._id,
+        };
+        const updatedRatings = [...book.ratings, newRating];
+        // Ajoute le newRating au tableau de notes existant
+
+        function calcAverageRating(ratings) {
+          // Calcule la note moyenne
+          const sumRatings = ratings(
+            (total, rate) => total + rate.grade,
+            0
+          );
+        
+
+        // Met à jour le document du livre
+        Book.findOneAndUpdate(
+          { _id: req.params.id, "ratings.userId": { $ne: user } }, // Recherche le livre avec l'identifiant donné et s'assure
+           //que l'utilisateur ne l'a pas déjà noté
+          // $ne = pas égal
+          {
+            $push: { ratings: newRating }, // Ajoutela nouvelle note au tableau des notes
+            averageRating: calcAverageRating(updatedRatings), // Met à jour le champ AverageRating
+          },
+          { new: true } // Renvoyez le document du livre mis à jour
+        )
+          .then((updatedBook) => res.status(201).json(updatedBook))
+          .catch((error) => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => res.status(401)({ error }))
+}
+};
