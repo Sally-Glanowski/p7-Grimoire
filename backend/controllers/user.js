228acb783f944742384ed392fe309026977ea894
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 16:27:03 +0200

    corrections

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -37,7 +37,7 @@ exports.login = (req, res, next) => {
             }),
           });
         })
-        .catch((error) => res.status(500).json({ error }))
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
