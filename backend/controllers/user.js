7e647107284d7d5af43f3428fbb552a12d4bfbd2
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 16:25:18 +0200

    corrections

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,7 +20,7 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.bdy.email })
+  User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
         return res.status(401).json({ error: "Utilisateur inconnu!" });
@@ -29,7 +29,7 @@ exports.login = (req, res, next) => {
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(400)({ error: "Mot de passe incorrect!" });
+            return res.status(401).json({ error: "Mot de passe incorrect!" });
           }
           res.status(200).json({
             userId: user._id,
