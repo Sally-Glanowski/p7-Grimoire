07d94e2feedc8c59db1fa42a56bf35e6ffbef974
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 16:17:10 +0200

    corrections

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -21,22 +21,23 @@ exports.getBestBooks = (req, res, next) => {
     .catch((error) => res.status(401).json({ error }));
 };
 exports.createBook = (req, res, next) => {
-    const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
-    delete bookobject._userId;
-    const book = new Book({
-      ..bookObject,
-      userId: req.auth.userId,
-      imageUrl: `${req.protocol}://{req.get("host")}/images/${
-        req.file.filename.split(".")[0]
-      }.webp`,
-      averageRating: bookObject.ratings[0].grade,
+  const bookObject = JSON.parse(req.body.book);
+  delete bookObject._id;
+  delete bookObject._userId;
+  const book = new Book({
+    ...bookObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename.split(".")[0]
+    }.webp`,
+    averageRating: bookObject.ratings[0].grade,
+  });
+  book
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Livre enregistré!" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
     });
-    book
-      .then(() => {
-        res.status(201).json({ message: "Livre enregistré !" });
-      })
-      .catch((error) => {
-        res.status(400).json({ error });
-      });
-  };
+};
