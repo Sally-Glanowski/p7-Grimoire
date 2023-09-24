146305e07b63697f6c7bb07f5450b3fb6d7501f4
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 16:23:18 +0200

    controllers user

---
+++ backend/controllers/user.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.signup = (req, res, next) => {
+  console.log(req);
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Nouvel utilisateur créé!" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.bdy.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur inconnu!" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(400)({ error: "Mot de passe incorrect!" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.TOKEN, {
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
