eb777caed6b027bffb9daadfd67871a0bb65edaf
Sally Glanowski <sally.glanowski@gmail.com>
2023-09-24 15:47:07 +0200

    controllers

---
+++ backend/controllers/book.js
@@ -0,0 +1,22 @@
+const Book = require("../models/book");
+const fs = require("fs");
+
+exports.getAllBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getSingleBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getBestBooks = (req, res, next) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(401).json({ error }));
+};
